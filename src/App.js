97d438456b89458131a9eb3f29c8a799115181e7
cukier-codes <cqr@o2.pl>
2021-01-18 17:29:47 +0100

    just hello button

--- src/App.js
+++ src/App.js
@@ -1,65 +1,47 @@
-import React, { Component } from "react";
+import React, {useState} from "react";
+import {Hello} from "./FetchRandomDog";
 import './App.css';
 
-class App extends Component {
+function App() {
+    const [showHello, setShowHello] = useState(false);
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            pics: [],
-            isLoaded: false,
-        }
-    }
 
-    componentDidMount() {
-        fetch('https://api.thedogapi.com/v1/images/search?api_key=525bdeb0-e806-4443-a50e-93404bf3709b')
-            .then(res => res.json())
-            .then(json => {
-                this.setState({
-                    pics: json,
-                    isLoaded: true
-                })
-            });
-    }
 
-    render() {
-        var {pics} = this.state;
-            return (
-                <div className="App">
-                    <div className="Top">
-                        <header className="Header">
-                            DOG API - get to know dogs!
-                        </header>
+    return (
+        <div className="App">
+            <div className="Top">
+                <header className="Header">
+                    DOG API - get to know dogs!
+                </header>
+            </div>
+            <div className="Mid">
+                <div className="Left">
+                    <div className="Form">
+                        <form method="get">
+                            <label>Choose a breed:</label><br/>
+                            <select id="breed" name="Breed">
+
+                            </select>
+                            <input type="submit" value="Submit"/>
+                        </form>
                     </div>
-                    <div className="Mid">
-                        <div className="Left">
-                            <div className="Form">
-                                <form method="get">
-                                    <label>Choose a breed:</label><br/>
-                                    <select id="breed" name="Breed">
-
-                                    </select>
-                                    <input type="submit" value="Submit"/>
-                                </form>
-                            </div>
-                        </div>
-                        <div className="Right">
-                            <div className="Button">
-                                <p>Not sure, what breed is for you?</p>
-                                <p>Go random!</p>
-                                <button id="Random">RANDOM</button>
-                            </div>
-                            <div className="Button-result">
-                                {pics.map(data => (
-                                    <img src={data.url} alt="dog" id="RandomDog"/>
-                                ))}
-                            </div>
-                        </div>
-                    </div>
-                    <div className="Mid-bot"></div>
-                    <div className="Bot"></div>
                 </div>
-            );
-        }
-    }
+                <div className="Right">
+                    <div className="Button">
+                        <p>Not sure, what breed is for you?</p>
+                        <p>Go random!</p>
+                        <button id="Random" onClick={() => setShowHello(!showHello)}>RANDOM</button>
+                    </div>
+                    <div className="Button-result">
+                        {showHello && <Hello/>}
+
+                    </div>
+                </div>
+            </div>
+            <div className="Mid-bot"></div>
+            <div className="Bot"></div>
+        </div>
+    )
+}
+
 export default App;
--- src/FetchRandomDog.js
+++ src/FetchRandomDog.js
@@ -1,24 +1,13 @@
 import React from "react";
 
-export default class FetchRandomDog extends React.Component {
-    state = {
-        loading: true,
-        dog: null
-    };
+export const Hello = () => {
+    React.useEffect(() => {
+        console.log("render");
+        return () => {
+            console.log("unmount");
+        };
 
-    async componentDidMount() {
-        const request = "https://api.thedogapi.com/v1/images/search?api_key=525bdeb0-e806-4443-a50e-93404bf3709b";
-        const response = await fetch(request);
-        const data = await response.json();
-        this.setState({dog: data.results[0], loading: false});
-    }
-    render() {
-        return (
-            <div>
+    }, []);
 
-                <img src={this.state.dog.url} alt="doggo"/>
-            </div>
-
-        );
-    }
+    return <div>HELLO</div>
 }
